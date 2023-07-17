280537a3da86267d4b25271c4e42d847d9b9d4c4
ivandelibasic <ivanr.delibasic@gmail.com>
2023-07-17 14:46:30 -0400

    I changed the fleet into Castor_Sprint2

--- src/test/java/com/Castor_Sprint2/pages/LoginPage.java
+++ src/test/java/com/Castor_Sprint2/pages/LoginPage.java
@@ -1,7 +1,8 @@
 package com.Castor_Sprint2.pages;
 
 
-import com.fleet.utilities.Driver;
+import com.Castor_Sprint2.utilities.Driver;
+import com.Castor_Sprint2.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -9,6 +10,7 @@ import org.openqa.selenium.support.PageFactory;
 public class LoginPage {
 
     public LoginPage(){
+
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
