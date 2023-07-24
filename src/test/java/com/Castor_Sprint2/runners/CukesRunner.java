2b8466fac012376042bfa5f283d5c7cf7d1c9541
DelibasicIvan <123925224+DelibasicIvan@users.noreply.github.com>
2023-07-23 22:36:39 -0400

    Update CukesRunner.java

--- src/test/java/com/Castor_Sprint2/runners/CukesRunner.java
+++ src/test/java/com/Castor_Sprint2/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/Castor_Sprint2/step_definitions",
         dryRun = false,
-        tags = "@US179_TeoDor",
+        tags = "",
 
         publish = true //generating a report with public link
 )
